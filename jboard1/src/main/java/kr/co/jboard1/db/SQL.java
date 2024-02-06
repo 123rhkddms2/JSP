71005bb8b9b243087c2e25c77aeecb4828c66843
123rhkddms2 <123rhkddms2@gmail.com>
2024-02-06 12:51:20 +0900

    글쓰기 데이터 입력까지 완료

--- jboard1/src/main/java/kr/co/jboard1/db/SQL.java
+++ jboard1/src/main/java/kr/co/jboard1/db/SQL.java
@@ -2,6 +2,7 @@ package kr.co.jboard1.db;
 
 public class SQL {
 
+	
 	public static final String SELECT_TERMS = "SELECT * FROM `terms`";
 	
 	public static final String INSERT_USER = "INSERT INTO `User` SET "
@@ -13,5 +14,16 @@ public class SQL {
 										   + "`hp`=?,"
 										   + "`regip`=?,"
 										   + "`rdate`=NOW()";
+	
+	public static final String SELECT_USER_FOR_LOGIN = "SELECT * FROM `User` WHERE `uid`=? AND `pass`=SHA2(?, 256)";
+	
+	
+	public static final String INSERT_ARTICLE = "INSERT INTO `Article` SET "
+												+ "`title`=?,"
+												+ "`content`=?,"
+												+ "`writer`=?,"
+												+ "`regip`=?,"
+												+ "`rdate`=NOW()";
+	
 	
 }
